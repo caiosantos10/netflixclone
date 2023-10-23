0636d9c91a7d51c30b1ff97971d445b899e3fdf2
Caio Santos <caiosant10@gmail.com>
2023-10-23 18:22:47 -0300

    Estilização do movierow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import './App.scss';
 import Tmdb from './Tmdb';
 import MovieRow from './components/movie-row/MovieRow';
 
--- src/components/movie-row/MovieRow.js
+++ src/components/movie-row/MovieRow.js
@@ -1,15 +1,20 @@
 import React from "react";
+import './MovieRow.scss';
 
 const url = `https://image.tmdb.org/t/p/w300`
 
 function MovieRow({ title, list }) {
     return (
-        <div>
+        <div className="movie-row">
             <h2> {title} </h2>
             <div className="movie-row--listarea">
-                {list.results.length > 0 && list.results.map((item, key)=>(
-                    <img key={key} src={`${url}${item.poster_path}`} alt="movie poster"></img>
-                ))}
+                <div className="movie-row__list">
+                    {list.results.length > 0 && list.results.map((item, key) => (
+                        <div key={key} className="movie-row__list--item">
+                            <img src={`${url}${item.poster_path}`} alt="movie poster"></img>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
