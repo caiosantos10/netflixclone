2342e65a81137937f6c052f12ffdf602190735cb
Caio Santos <caiosant10@gmail.com>
2023-10-23 17:53:35 -0300

    Aplying css on App component

--- src/components/movie-row/MovieRow.js
+++ src/components/movie-row/MovieRow.js
@@ -8,7 +8,7 @@ function MovieRow({ title, list }) {
             <h2> {title} </h2>
             <div className="movie-row--listarea">
                 {list.results.length > 0 && list.results.map((item, key)=>(
-                    <img src={`${url}${item.poster_path}`} alt="movie poster"></img>
+                    <img key={key} src={`${url}${item.poster_path}`} alt="movie poster"></img>
                 ))}
             </div>
         </div>
