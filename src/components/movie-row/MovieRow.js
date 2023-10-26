95e3cc9091a1e596f3b957056316f07a5b1ce932
Caio Santos <caiosant10@gmail.com>
2023-10-26 13:56:01 -0300

    Resolving dependencies conflicts

--- src/components/movie-row/MovieRow.js
+++ src/components/movie-row/MovieRow.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './MovieRow.scss';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 const url = `https://image.tmdb.org/t/p/w300`
 
@@ -7,6 +9,12 @@ function MovieRow({ title, list }) {
     return (
         <div className="movie-row">
             <h2> {title} </h2>
+            <div className="movie-row--left">
+                <NavigateBeforeIcon style={{fontSize: 50}}/>
+            </div>
+            <div className="movie-row--right">
+                <NavigateNextIcon style={{fontSize: 50}}/>
+            </div>
             <div className="movie-row--listarea">
                 <div className="movie-row__list">
                     {list.results.length > 0 && list.results.map((item, key) => (
